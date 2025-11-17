93312cf4cb6ba121838d98ee9edb06647fabe3ac
Magor Pocsveiler <mp1523@ic.ac.uk>
2025-11-17 19:50:36 +0000

    Added current settings to record setup page. The Current Settings are also added to save file and the settigns are set automatically in load file.

--- src/main/java/ibm/gui/GUIController.java
+++ src/main/java/ibm/gui/GUIController.java
@@ -7,6 +7,7 @@ import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
 import java.io.*;
 import java.util.Arrays;
+import java.util.StringJoiner;
 import java.util.logging.*;
 
 public class GUIController {
@@ -68,7 +69,6 @@ public class GUIController {
         //Switches to startup page
         menuBar.getMenu(0).setVisible(true);
         menuBar.getMenu(1).setVisible(true);
-        //menuBar.getMenu(1).getItem(0).setVisible(true);
         this.cards_switcher.show(cards, "monitor");
 
         return true;
@@ -90,6 +90,25 @@ public class GUIController {
         setupMenu.add(setupItem);
         menuBar.add(setupMenu);
 
+        JMenu monitorMenu = createMonitorMenu();
+
+        menuBar.add(monitorMenu);
+
+        JMenuItem helpMenu = new JMenuItem("Help");
+        helpMenu.addActionListener(_->{
+           JOptionPane helpMenuOptionPane = new JOptionPane();
+           
+           helpMenuOptionPane.setMessage("<html><body><p style='width: 200px;'>"+"There ain't no help where you're looking and now this is just testing if the " +
+                   "thing will wrap because it should but im not 100% sure it will because Ive never tried this before really and it would be very cool if it did." +
+                   " Obviously this is yappery and absolutely useless but ehhhhhh"+"</p></body></html>"); //TODO Actual HELP
+            helpMenuOptionPane.setFont(f);
+           JOptionPane.showMessageDialog(helpMenuOptionPane,helpMenuOptionPane.getMessage(),"Help",JOptionPane.INFORMATION_MESSAGE);
+        });
+        menuBar.add(helpMenu);
+        return menuBar;
+    }
+
+    private JMenu createMonitorMenu() {
         JMenu monitorMenu = new JMenu("Monitor");
         JMenuItem graphClearItem = new JMenuItem("Clear all Graphs");
         //graphClearItem.setVisible(false);
@@ -100,64 +119,91 @@ public class GUIController {
         });
         monitorMenu.add(graphClearItem);
         JMenuItem saveGraph = new JMenuItem("Save Graph");
-        saveGraph.addActionListener(_->{
-            JFileChooser chooser = new JFileChooser();
-            chooser.setDialogTitle("Save As");
-            chooser.setSelectedFile(new File("untitled.csv"));
-            if(chooser.showSaveDialog(null) == JFileChooser.APPROVE_OPTION) {
-                File file = chooser.getSelectedFile();
-                try {
-                    FileWriter fw = new FileWriter(file);
-                    BufferedWriter bw = new BufferedWriter(fw);
-                    bw.write("Hello World");
-                    bw.close();
-                    //TODO ADD SAVE GRAPH
-                    System.out.println("Saved file:" + file.getName() + "  To:" + file.getAbsolutePath());
-                } catch(Exception e) {
-                    System.out.println(e.getMessage());
-                }
-            }
-        });
+        saveGraph.addActionListener(_-> saveFileDialog());
         monitorMenu.add(saveGraph);
 
         JMenuItem loadGraph = new JMenuItem("Load Graph");
-        loadGraph.addActionListener(_->{
-            JFileChooser chooser = new JFileChooser();
-            chooser.setDialogTitle("Load Graph");
-            chooser.setFileFilter(new FileNameExtensionFilter("CSV Files","csv"));
-           if(chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
-               File file = chooser.getSelectedFile();
-               if(file.exists() && !file.isDirectory() && file.canRead() && file.getName().toLowerCase().endsWith(".csv")) {
-                   try {
-                       FileReader fr = new FileReader(file);
-                       //TODO Impletment file reader
-                   } catch (Exception e) {
-                       System.out.println(e.getMessage());
-                   }
-               }
-               else {
-                   JOptionPane.showMessageDialog(null, "Please Select a CSV File", "Warning", JOptionPane.WARNING_MESSAGE);
-               }
-           }
+        loadGraph.addActionListener(_-> {
+            if (JOptionPane.showConfirmDialog(null, "Do you want to load a new file? \nThis will clear all graphs.", "Warning", JOptionPane.YES_NO_OPTION) == 0)
+                loadFileDialog();
         });
         monitorMenu.add(loadGraph);
 
         monitorMenu.setVisible(false);
+        return monitorMenu;
+    }
 
-        menuBar.add(monitorMenu);
+    private void loadFileDialog(){
+        JFileChooser chooser = new JFileChooser();
+        chooser.setDialogTitle("Load Graph");
+        chooser.setFileFilter(new FileNameExtensionFilter("CSV Files","csv"));
+        if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+            File file = chooser.getSelectedFile();
+            if (file.exists() && !file.isDirectory() && file.canRead() && file.getName().toLowerCase().endsWith(".csv")) {
+                loadFile(file);
 
-        JMenuItem helpMenu = new JMenuItem("Help");
-        helpMenu.addActionListener(_->{
-           JOptionPane helpMenuOptionPane = new JOptionPane();
-           
-           helpMenuOptionPane.setMessage("<html><body><p style='width: 200px;'>"+"There ain't no help where you're looking and now this is just testing if the " +
-                   "thing will wrap because it should but im not 100% sure it will because Ive never tried this before really and it would be very cool if it did." +
-                   " Obviously this is yappery and absolutely usless but ehhhhhh"+"</p></body></html>"); //TODO Actual HELP
-            helpMenuOptionPane.setFont(f);
-           JOptionPane.showMessageDialog(helpMenuOptionPane,helpMenuOptionPane.getMessage(),"Help",JOptionPane.INFORMATION_MESSAGE);
-        });
-        menuBar.add(helpMenu);
-        return menuBar;
+            } else {
+                JOptionPane.showMessageDialog(null, "Please Select a CSV File", "Warning", JOptionPane.WARNING_MESSAGE);
+                loadFileDialog();
+            }
+        }
+
+    }
+    private void loadFile(File file){
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String settingsLine = br.readLine();
+            String[] settings = settingsLine.split(",");
+            int numWaveForms = Integer.parseInt(settings[0]);
+            int sampleRate = Integer.parseInt(settings[1]);
+            AppState.currentSettings = new SetupSettings(numWaveForms, sampleRate);
+            return;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    private void saveFileDialog(){
+        JFileChooser chooser = new JFileChooser();
+        chooser.setDialogTitle("Save As");
+        chooser.setSelectedFile(new File("untitled.csv"));
+        if(chooser.showSaveDialog(null) == JFileChooser.APPROVE_OPTION) {
+            File file = chooser.getSelectedFile();
+            if(!file.exists()){
+                writeFile(file);
+            }
+            else {
+                switch(JOptionPane.showConfirmDialog(null, "File already exists. "
+                        + "\n Do you want to overwrite the file", "Warning", JOptionPane.YES_NO_CANCEL_OPTION)){
+                    case JOptionPane.YES_OPTION:
+                        try {
+                            writeFile(file);
+                            return;
+                        } catch(Exception e) {
+                            System.out.println(e.getMessage());
+                        }
+                        return;
+                    case JOptionPane.NO_OPTION:
+                        saveFileDialog();
+
+                    case JOptionPane.CANCEL_OPTION:
+                }
+            }
+        }
+    }
+    private void writeFile(File file){
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter(file));
+            StringJoiner setupLine = new StringJoiner(",");
+            setupLine.add("" + AppState.currentSettings.numWaveForms);
+            setupLine.add("" + AppState.currentSettings.sampleRate);
+            setupLine.add("-->");
+            bw.write(setupLine.toString());
+            bw.close();
+            //TODO ADD SAVE GRAPH
+            System.out.println("Saved file:" + file.getName() + "  To:" + file.getAbsolutePath());
+        }
+        catch (IOException e) {
+            logger.log(Level.WARNING, "Could not save file: " + file.getName(), e);
+        }
     }
 
     private void createLandingPage(){
@@ -179,7 +225,15 @@ public class GUIController {
         //Create and add the setup page
         SetupPage setupPage = new SetupPage();
         cards.add(setupPage.getPanel(), "setup");
+
+        setupPage.getButton_loadSetup().addActionListener(_ -> {
+            if(JOptionPane.showConfirmDialog(null, "Would you like to load a pre-existing file?", "Load File?", JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) {
+                loadFileDialog();
+                showMonitorPage();
+            }
+        });
         setupPage.getButton_finish_setup().addActionListener(_ -> {
+            AppState.currentSettings = new SetupSettings(setupPage.getNumWaveForms(), setupPage.getSampleRate());
             if(!this.showMonitorPage()) {
                 try {
                     throw new Exception("Switch to Monitor Page Failed");
@@ -196,5 +250,9 @@ public class GUIController {
         monitorPage.getGraphsPane().setDividerLocation(frameSize.height/2-100);
         cards.add(monitorPage.getPanel(), "monitor");
     }
+    public static class AppState {
+        public static SetupSettings currentSettings;
+    }
 
+    public record SetupSettings(int numWaveForms, int sampleRate) {}
 }
