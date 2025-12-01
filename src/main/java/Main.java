7a63b0261e11b308e82a3cdcc5f79eec2947d5a9
Magor Pocsveiler <mp1523@ic.ac.uk>
2025-12-01 15:11:12 +0000

    Bug fix

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,34 +1,31 @@
-
 import ibm.gui.GUIController;
-
 import javax.swing.*;
 import java.awt.*;
-import java.util.logging.ConsoleHandler;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import com.formdev.flatlaf.*;
+import java.util.logging.*;
 
-private static final Logger guiLogger = Logger.getLogger(GUIController.class.getName());
+public class Main {
 
-void main() {
-    setupLogger(guiLogger);
-    SwingUtilities.invokeLater(() -> {
-        try {
-            GUIController gui = new GUIController(guiLogger);
-            if (!gui.init()) throw new ExceptionInInitializerError("GUI Initialization Error");
-            gui.showGUI();
-        } catch (Exception e) {
-            IO.print("Failed to Initialize GUI");
-        }
-    });
+    private static final Logger guiLogger =
+            Logger.getLogger(GUIController.class.getName());
 
-}
+    public static void main(String[] args) {
+        setupLogger(guiLogger);
+        SwingUtilities.invokeLater(() -> {
+            try {
+                GUIController gui = new GUIController(guiLogger);
+                if (!gui.init()) throw new ExceptionInInitializerError("GUI Initialization Error");
+                gui.showGUI();
+            } catch (Exception e) {
+                IO.print("Failed to Initialize GUI");
+            }
+        });
+    }
 
-private static void setupLogger(Logger logger) {
-    ConsoleHandler handler = new ConsoleHandler();
-    handler.setLevel(Level.ALL);              // logs everything to console
-
-    logger.addHandler(handler);            // attach handler to logger
-    logger.setUseParentHandlers(false);    // avoid duplicate console output
-    logger.setLevel(Level.ALL);            // accept all log levels
+    private static void setupLogger(Logger logger) {
+        ConsoleHandler handler = new ConsoleHandler();
+        handler.setLevel(Level.ALL);
+        logger.addHandler(handler);
+        logger.setUseParentHandlers(false);
+        logger.setLevel(Level.ALL);
+    }
 }
